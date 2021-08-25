c267f2a73daf4ced2bcb96890b15ec089cbdb478
DanyMilan23 <esteban239222@gmail.com>
2021-08-24 21:30:36 -0400

    initial commit with header

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import { Component } from "../lib/react/index.js";
+import styled from "../lib/styled-components.js";
+import Header from "./header.js";
+const AppStyled = styled.div``;
+
+class App extends Component {
+  render() {
+    return AppStyled({ children: [new Header()] }, "Hello world");
+  }
+}
+
+export default App;
